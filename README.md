2258575c7ad08025ce902bcd5755a78e17cbbb45
Carlton Joseph <carlton.joseph@gmail.com>
2018-11-20 14:57:00 -0800

    code without pwa

---
+++ README.md
@@ -0,0 +1,5 @@
+# Progressive Web Apps With Workbox
+
+The code in this repository is based on the
+[PWA series](https://www.youtube.com/watch?v=Nduh3IUtyrE&list=PLIiQ4B5FSuphk6P-zg_E3W9zL3J22U4dT&index=2)
+video serie tutorial.
---
+++ app.js
@@ -0,0 +1,14 @@
+const attendees = document.querySelector("#attendees");
+console.log("att", attendees);
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json())
+  .then(data => {
+    let html = "";
+    data.forEach(
+      user =>
+        (html += `<div class="card"><h2>${user.name}</h2><div>${
+          user.email
+        }</div></div>`)
+    );
+    attendees.innerHTML = html;
+  });
