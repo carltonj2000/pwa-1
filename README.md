2ce6ef4fe3011253e1baa7a117cbb478ddb8c930
Carlton Joseph <carlton.joseph@gmail.com>
2018-11-20 16:06:42 -0800

    add basic workbox code and flow

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Progressive Web Apps With Workbox
 
+Use the following command to setup workbox:
+
+```
+npx workbox wizard # used defaults but only cachce css, js & html
+npx workbox generateSW workbox-config.js
+```
+
+## History
+
 The code in this repository is based on the
 [PWA series](https://www.youtube.com/watch?v=Nduh3IUtyrE&list=PLIiQ4B5FSuphk6P-zg_E3W9zL3J22U4dT&index=2)
 video serie tutorial.
--- sw.js
+++ sw.js
@@ -1,16 +1,40 @@
-self.addEventListener("install", event => {
-  console.log("sw installed");
-  event.waitUntil(
-    caches.open("precache-v1").then(cache => {
-      cache.addAll(["/", "app.js"]);
-    })
-  );
-});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "app.css",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
+    "url": "app.js",
+    "revision": "2d4f61cde2f377ae8c08c1eb54a7e332"
+  },
+  {
+    "url": "index.html",
+    "revision": "100d38d90028774105c5cec674c5e483"
+  },
+  {
+    "url": "workbox-config.js",
+    "revision": "2a5130c5c0adbb75c11ff6636f008fb6"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
